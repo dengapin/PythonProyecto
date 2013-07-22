6696c952764d3bd6f31bb299191b33d42c96361a
dennise <dengapin@gmail.com>
2013-07-22 15:40:02 -0500

    Prueba de IDE Ninja colocando nuevo icono a ventana

--- PruebaIDENinja/main.py
+++ PruebaIDENinja/main.py
@@ -1,11 +1,14 @@
 # -*- coding: utf-8 -*-
 import sys
 from PyQt4.QtGui import QApplication
+from PyQt4 import QtGui
 from PyQt4 import uic
 
 app= QApplication(sys.argv)
 
 w= uic.loadUi('/Users/Dennise/Desktop/ProyectoLP.ui')
-w.show()
 
+w.setWindowIcon(QtGui.QIcon('icono.jpg'))
+
+w.show()
 sys.exit(app.exec_())
