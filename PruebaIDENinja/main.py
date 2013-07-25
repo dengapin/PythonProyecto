e2f68689c3db59eeff5a2b013c7db5cc30fea622
dennise <dengapin@gmail.com>
2013-07-24 21:31:28 -0500

    pruebas

--- PruebaIDENinja/main.py
+++ PruebaIDENinja/main.py
@@ -8,6 +8,7 @@ app= QApplication(sys.argv)
 
 w= uic.loadUi('/Users/Dennise/Desktop/ProyectoLP.ui')
 w.setGeometry(300,300,250,150)
+w.setWindowTitle('Proyecto')
 w.setWindowIcon(QtGui.QIcon('icono.jpg'))
 
 w.show()
