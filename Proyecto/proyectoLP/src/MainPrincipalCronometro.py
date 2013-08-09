fd0cc8ce2864cc1568f7e63396374aeec7bf694f
dennise <dengab_19@hotmail.com>
2013-08-09 14:38:42 -0500

    cambio de cronometro

--- Proyecto/proyectoLP/src/MainPrincipalCronometro.py
+++ Proyecto/proyectoLP/src/MainPrincipalCronometro.py
@@ -6,6 +6,179 @@ Created on 31/07/2013
 '''
 
 import sys,os
+'''
+Created on 31/07/2013
+
+@author: Figueroa, Pintado
+'''
+
+import sys,os
+from PyQt4 import QtCore, QtGui, uic
+from numero import *
+from ventanaSudoku1 import Ui_prueba
+    
+class Main(QtGui.QMainWindow):
+       
+    def __init__(self):
+        QtGui.QWidget.__init__(self)
+        self.ui = Ui_prueba()
+        self.ui.setupUi(self)
+        '''Variables Globales para el cronometro '''
+        self.tiempo = QtCore.QTime()
+        self.timer = QtCore.QTimer()
+        self.nuevoTiempo = QtCore.QTime()
+        self.startTime()
+        self.initGui()
+        #self.timer = QtCore.QTimer(self)
+        #self.timer.timeout.connect(self.showlcd)
+        ###connect(timer,self.SIGNAL(timer.timeout()),self,QtCore.SLOT(self.startTime()))
+        #self.timer.start(1000)
+        #self.lcd.display("00:00")    
+              
+    def initGui(self):        
+        global coorX
+        global coorY
+        global encriptar
+        global celdaSeleccionada
+        global casilla
+        global valorPad
+        
+        casilla = [[0 for j in range(9)] for i in range(9)]
+        valorPad = [0 for j in range(9)]
+        coorX = -1
+        coorY = -1
+        
+        flag = False
+                  
+        for i in range(0,9):    
+            for j in range(0,9):
+               celda = numero()
+               celda.setFila(i)
+               celda.setColumna(j)
+               celda.setNumReal(0)
+               celda.setNumAsignado(0)
+               celda.setEsConstante(False)
+               
+               if(flag):
+                   celda.setColor(0)
+                   celda.setStyleSheet('QPushButton {background-color: red}' )
+               else:
+                   celda.setColor(1)
+                   celda.setStyleSheet('QPushButton {background-color: blue}' )
+               casilla [i][j]= celda
+               self.ui.taberosudoku.addWidget(celda,i,j)
+               
+               if(j % 3 == 2):
+                    flag = not(flag)
+            if(i % 3 != 2):
+                flag = not (flag)
+                  
+        for i in range(0,9):
+            numSelec = QPushButton(str(i+1))
+            valorPad [i] = numSelec
+            self.ui.tableronumeros.addWidget(numSelec,i/3, i%3)
+            numSelec.setStyleSheet('QPushButton {background-color: black; color: white}')
+    
+        self.ui.blimpiar.setText('Borrar')
+        self.ui.blimpiar.setEnabled(False)
+        celdaSeleccionada = False
+        self.ventanaInvalida()
+        
+                     
+   
+               
+    def startTime(self):      
+        self.tiempo.setHMS(0,0,0,0)
+        self.connect(self.timer,QtCore.SIGNAL("timeout()"),self.showlcd)
+        self.segundos = 0
+        self.minutos =0
+        self.text = self.tiempo.toString("hh:mm:ss")
+        self.ui.lcdNumber.display(self.text)
+        self.timer.start(1000) 
+        
+    
+    def showlcd(self):
+        self.segundos = self.segundos + 1
+        self.nuevoTiempo = self.tiempo.addSecs(self.segundos)
+        self.cronometro = self.nuevoTiempo.toString("hh:mm:ss")
+        self.ui.lcdNumber.display(self.cronometro)     
+        
+            
+    def ventanaInvalida(self):
+        coorX = -1
+        coorY = -1
+        celda = numero()
+        for i in range(0,9):
+            for j in range(0,9):
+                celda = casilla[i][j]
+                celda.setEnabled(False)
+                if(celda.getColor() == 0):
+                    celda.setStyleSheet('QPushButton {background-color: red}')
+                else:
+                    celda.setStyleSheet('QPushButton {background-color: blue}')
+            numSelec = QPushButton()
+            numSelec = valorPad[i]
+            numSelec.setEnabled(False)
+        
+        self.ui.blimpiar.setEnabled(False)
+        self.ui.bnormal.setChecked(False)
+        self.celdaSeleccionada = False
+        
+        self.ui.groupBox_2.setEnabled(False)        
+                    
+    def celdaClicked(self):
+        if(not(celdaSeleccionada)):
+            celdaSeleccionada = True
+        else:
+            celda = casilla[coorX][coorY]
+            if(celda.getColor == 0):
+                celda.setStyleSheet('QPushButton {background-color: red}')
+            else:
+                celda.setStyleSheet('QPushButton {background-color: blue}')
+        celda = numero()
+        celda.sender()
+        coorX = celda.getFila()
+        coorY = celda.getColumna()
+        
+        if (celda.getNumReal() == celda.getNumeroAsignado()):
+            self.ui.bnormal.setChecked(True)
+        else:
+            self.ui.bnormal.setChecked(False)
+            numSelec = QPushButton() 
+        for i in range(0,9):
+            numSelec = valorPad[i] 
+            numSelec.setEnabled(True)
+        if(self.ui.bnormal.isChecked()):
+            self.validarCelda()
+        
+        celda.setStyleSheet('QPushButton {background-color: orange}')
+            
+    def validarCelda(self):
+        centerX = coorX/3
+        centerY = coorY/3
+        celda = numero()
+        for i in range(0,9):
+            for j in range(0,9):
+                if(i == coorX | j == coorY | (i/3 == centerX & j/3 == centerY )):
+                    celda = casilla[i][j]
+                    if (celda.getNumeroAsignado()):
+                        numSelec = QPushButton()
+                        valor = celda.getNumeroAsignado()
+                        numSelec = valorPad[valor -1]
+                        numSelec.setEnabled(False)
+        
+        celda = casilla[coorX][coorY]
+        if(not(celda.getNumeroAsignado())):
+            self.ui.blimpiar.setEnabled(False)
+        else:
+            self.ui.blimpiar.setEnabled(True)
+            
+if __name__ == "__main__":
+    app= QtGui.QApplication(sys.argv)
+    window = Main()
+    window.show()
+    sys.exit(app.exec_())
+
 from PyQt4 import QtCore, QtGui, uic
 from numero import *
 from ventanaSudoku import Ui_prueba
